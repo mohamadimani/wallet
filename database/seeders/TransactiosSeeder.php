bec98d1e0fae15a015ec1a0d1a5b1002a6299c9c
manimohamadi <mohamadimahdi@yahoo.com>
2023-10-10 12:31:53 +0330

    add migration

---
+++ database/seeders/TransactiosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TransactiosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}

3ee172fe594575f9dc78fd5a0a94d055b6e0d3df
manimohamadi <mohamadimahdi@yahoo.com>
2023-10-22 13:41:47 +0330

    add blade view

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return  view('index');
 });
 
-require __DIR__.'/auth.php';
